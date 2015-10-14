388ef5cd1fdae47c826609390dd41410913cb39a
fabianrios <hola@fabianrios.co>
2015-10-14 13:40:08 -0500

    fix app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,4 @@
 //= require angular-route/angular-route
 //= require angular-resource/angular-resource
 //= require angular-rails-templates
-//= require angular-rails-templates
 //= require_tree .
