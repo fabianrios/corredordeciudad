45bb5489e87d50d7e30f3285b3bc549b2fb4fc4a
fabianrios <hola@fabianrios.co>
2015-10-15 16:02:20 -0500

    quitar la entrada sin registro

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -1,6 +1,6 @@
 class EventosController < ApplicationController
   before_action :set_evento, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
   # GET /eventos
   # GET /eventos.json
   def index
