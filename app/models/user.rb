05d46664e2de2cf28c15d6a664b1cc386abfaafd
fabianrios <hola@fabianrios.co>
2015-10-15 13:41:16 -0500

    bien la tarea

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :eventos, dependent: :destroy
   
+  mount_uploader :logo, LogoUploader
+  mount_uploader :imagen, PicUploader
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
